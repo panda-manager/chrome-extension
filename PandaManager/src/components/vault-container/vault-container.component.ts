fe4e827f01e9a08cd79a7e6560818e33571e95c3
Nadav Shamir <nadavsh@mta.ac.il>
2024-05-09 18:05:33 +0300

    search in vault

--- PandaManager/src/components/vault-container/vault-container.component.ts
+++ PandaManager/src/components/vault-container/vault-container.component.ts
@@ -1,24 +1,38 @@
-import { Component, Host, OnInit } from '@angular/core'
+import { ChangeDetectorRef, Component, Host, OnInit } from '@angular/core'
 import { HttpService } from '../../services/http.service'
 import { DisplayedCredential } from '../../models/contracts/displayed-credentials-response'
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner'
 import { MatExpansionModule } from '@angular/material/expansion'
-import { FormsModule } from '@angular/forms'
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms'
 import { MatDialog } from '@angular/material/dialog'
 import { VaultCredentialDialogComponent } from '../vault-credentials-dialog/vault-credentials-dialog.component'
+import { MatInputModule } from '@angular/material/input'
+import { debounceTime } from 'rxjs'
+import { MatFormFieldModule } from '@angular/material/form-field'
+import { MatIconModule } from '@angular/material/icon'
 
 @Component({
   selector: 'app-vault-container',
   templateUrl: './vault-container.component.html',
   styleUrls: ['./vault-container.component.css'],
   standalone: true,
-  imports: [MatProgressSpinnerModule, MatExpansionModule, FormsModule],
+  imports: [
+    MatProgressSpinnerModule,
+    MatExpansionModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatIconModule,
+  ],
 })
 export class VaultContainerComponent implements OnInit {
+  displayedCredentialsFromServer: DisplayedCredential[]
   displayedCredentials: DisplayedCredential[]
   isLoading = false
   name = 'Angular'
   shownPasswordDict: Record<string, string> = {}
+  searchControl = new FormControl()
 
   constructor(
     private http: HttpService,
@@ -28,9 +42,20 @@ export class VaultContainerComponent implements OnInit {
   ngOnInit() {
     this.isLoading = true
     this.http.getDisplayedCredentials().subscribe((displayedCredentials) => {
+      this.displayedCredentialsFromServer = displayedCredentials
       this.displayedCredentials = displayedCredentials
       this.isLoading = false
     })
+
+    this.searchControl.valueChanges
+      .pipe(debounceTime(100))
+      .subscribe((value) => {
+        this.displayedCredentials = this.displayedCredentialsFromServer.filter(
+          (displayedCredentials) =>
+            displayedCredentials.host.includes(value) ||
+            displayedCredentials.login.includes(value)
+        )
+      })
   }
 
   showPasswordClicked(displayedCredential: DisplayedCredential) {
