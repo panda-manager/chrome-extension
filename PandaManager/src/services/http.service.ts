4238f0a4033e2845322b74b6b5d43b62018db795
Nadav Shamir <nadavsh@mta.ac.il>
2024-05-09 19:35:43 +0300

    .

--- PandaManager/src/services/http.service.ts
+++ PandaManager/src/services/http.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http'
 import { Host, Injectable } from '@angular/core'
-import { Observable, delay, of } from 'rxjs'
+import { Observable, delay, map, of } from 'rxjs'
 import { DisplayedCredential } from '../models/contracts/displayed-credentials-response'
 
 @Injectable({
@@ -9,17 +9,18 @@ import { DisplayedCredential } from '../models/contracts/displayed-credentials-r
 export class HttpService {
   constructor(private http: HttpClient) {}
 
-  public login(email: string, password: string): Observable<string> {
-    this.http.post(
-      'http://localhost:8080/auth/login',
-      {
-        email,
-        password,
-      },
-      { responseType: 'text' }
-    )
-
-    return of('token mock')
+  public login(email: string, master_password: string): Observable<string> {
+    return this.http
+      .post(
+        'http://localhost:8080/auth/login',
+        {
+          email,
+          master_password,
+        },
+        { responseType: 'json' }
+      )
+      .pipe(map((res) => res['access_token']))
+    // return of('token mock')
   }
 
   public register(
@@ -32,11 +33,13 @@ export class HttpService {
       'http://localhost:8080/auth/register',
       {
         email,
-        password,
-        firstName,
-        lastName,
+        master_password: password,
+        first_name: firstName,
+        last_name: lastName,
       },
-      { responseType: 'text' }
+      {
+        responseType: 'text',
+      }
     )
 
     // return of('token mock')
